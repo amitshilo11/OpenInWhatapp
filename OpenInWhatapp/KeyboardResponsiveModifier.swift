7c73d35e12521282dbc61b541022428a4c7ae59d
AmitShilo <amitshi@assacnetworks.com>
2024-04-18 09:07:02 +0300

    first commit

---
+++ OpenInWhatapp/KeyboardResponsiveModifier.swift
@@ -0,0 +1,8 @@
+//
+//  KeyboardResponsiveModifier.swift
+//  OpenInWhatapp
+//
+//  Created by Amit Shilo on 07/04/2024.
+//
+
+import Foundation
---
+++ OpenInWhatapp/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  OpenInWhatapp
+//
+//  Created by Amit Shilo on 06/04/2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
